22bc33053b146e7fcac24734a83cec69e997a8a5
Dr.MagPie <ShadowSega15@gmail.com>
2020-09-03 14:34:06 +0300

    Simplified Media Handeling

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-docker run --rm -v="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/minimal:latest jekyll serve --drafts
+docker run --rm -v="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll:latest jekyll serve --drafts
