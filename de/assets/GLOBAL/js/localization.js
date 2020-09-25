284facc07d83ac096cd53da2fc2a1596fb61ed0e
Conor12345 <Conor12345@users.noreply.github.com>
2020-09-25 12:32:02 +0000

    deploy: b7320b3862d3bb2c7ff86f46dad88d5058309823

---
+++ de/assets/GLOBAL/js/localization.js
@@ -0,0 +1,18 @@
+function getLanguage() {
+  return localStorage.getItem("language") || 'en';
+}
+
+function setLanguage(lang) {
+  if (lang === undefined || lang === '') { // make sure that the lang parameter was properly passed in
+    return 1
+  }
+  if (typeof lang !== 'string') {
+    throw new typeError("argument 'lang' must be a string");
+  }
+  if (lang.length !== 2) { // language abbreviations are 2 letters long
+    return 1
+  }
+
+  localStorage.setItem('language', lang);
+  return 0
+}
